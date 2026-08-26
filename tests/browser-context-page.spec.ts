211096962d2d2e27a6964021c658e7892700448d
Анна Козырева <kozyreva.hanna@gmail.com>
2026-08-26 15:25:51 +0200

    add test with browser-context-page fixture

---
+++ tests/browser-context-page.spec.ts
@@ -0,0 +1,20 @@
+import { test, chromium, Browser, BrowserContext, Page } from '@playwright/test'
+
+test('Browser, context, page', async () => {
+  // create browser
+  const browser: Browser = await chromium.launch()
+  console.log(browser.contexts())
+  // create context
+  const context1: BrowserContext = await browser.newContext()
+  const context2: BrowserContext = await browser.newContext()
+
+  // create page
+  const page1: Page = await context1.newPage()
+  const page2: Page = await context2.newPage()
+  page1.goto('https://www.saucedemo.com/')
+  page2.goto('https://www.saucedemo.com/')
+  await page1.waitForTimeout(5000)
+  await page2.waitForTimeout(5000)
+  await context1.close()
+  await context2.close()
+})
